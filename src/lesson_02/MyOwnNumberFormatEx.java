e40110395b848d64ec9801c76dbcaf5235adf802
madlur <59394178+madlur@users.noreply.github.com>
2020-04-13 13:05:07 +0300

    Delete MyOwnNumberFormatEx.java

--- src/lesson_02/MyOwnNumberFormatEx.java
+++
@@ -1,8 +0,0 @@
-package lesson_02;
-
-public class MyOwnNumberFormatEx extends Exception { //наследуюсь от класса Exception а не от непроверяемых Runtime
-                                                    // поэтому в мейне метод требует обработки try/catch или проброс
-    MyOwnNumberFormatEx(){
-        System.out.println("Your array contains NaN");
-    }
-}
