ec621a25ff05be4aefe59da0d9f930a0c7ac9ebc
Anton Sobolev <2185430@gmail.com>
2020-04-15 15:09:55 +0300

    homework_lesson04.upload_lesson_files

---
+++ src/lesson_04/ChatServer.java
@@ -0,0 +1,12 @@
+package lesson_04;
+
+public class ChatServer {
+    public void start(int port) {
+        System.out.println("Server started at port: " + port);
+    }
+
+    public void stop() {
+        System.out.println("Server stopped");
+    }
+
+}
