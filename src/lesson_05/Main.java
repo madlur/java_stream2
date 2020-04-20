99fbf535a64bdda918528ac68c47515dac50eb7a
Anton Sobolev <2185430@gmail.com>
2020-04-20 17:08:58 +0300

    homework_lesson05.v.2.1_correcting_mistakes

--- src/lesson_05/Main.java
+++ src/lesson_05/Main.java
@@ -1,17 +1,17 @@
 package lesson_05;
 
 public class Main {
+    static final Object mon = new Object();
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
         method1();
         method2();
 
     }
 
-    private static void method1() {
+    private synchronized static void method1() {
         final int size = 10000000;
-        final int h = size / 2;
         float[] arr = new float[size];
 
         for (int i = 0; i < arr.length; i++) {
@@ -22,9 +22,10 @@ public class Main {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
         System.out.println("Working time of method1 is: " + (System.currentTimeMillis() - a) + " milliseconds");
+
     }
 
-    static void method2() throws InterruptedException {
+    static void method2() {
 
         final int size = 10000000;
         final int h = size / 2;
@@ -38,35 +39,27 @@ public class Main {
         System.arraycopy(arr, 0, arr1, 0, h);
         System.arraycopy(arr, h, arr2, 0, h);
 
-
         Runnable r = new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < arr1.length; i++) {
-                    arr1[i] = (float) (arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-                }
-            }
-        };
-        Runnable r2 = new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < arr2.length; i++) {
-                    arr2[i] = (float) (arr2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-                }
 
             }
         };
-        Thread tr1 = new Thread(r, "Thread#1");
-        tr1.start();
-        Thread tr2 = new Thread(r2, "Thread#2");
-        tr2.start();
+        new Thread(r, "1").start();
+        new Thread(r, "2").start();
+        synchronized (mon) {
+            for (int i = 0; i < arr1.length; i++) {
+                arr1[i] = (float) (arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
 
-        tr1.join();
-        tr2.join();
+            for (int i = 0; i < arr2.length; i++) {
+                arr2[i] = (float) (arr2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
 
-        System.arraycopy(arr1, 0, arr, 0, h);
-        System.arraycopy(arr2, 0, arr, h, h);
+            System.arraycopy(arr1, 0, arr, 0, h);
+            System.arraycopy(arr2, 0, arr, h, h);
 
-        System.out.println("Working time of method2 is: " + (System.currentTimeMillis() - a) + " milliseconds");
+            System.out.println("Working time of method2 is: " + (System.currentTimeMillis() - a) + " milliseconds");
+        }
     }
 }
